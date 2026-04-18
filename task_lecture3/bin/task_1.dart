880a8fc955d5b04458eb5e451dad48ce18f43b6b
kaReem Alaa <kareemalaabakry@gmail.com>
2026-04-18 22:39:49 +0200

    Edit A constant variable whose value & A simple calculation

--- task_lecture3/bin/task_1.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:io';
-
-void main() {
-  int num;
-  num = int.parse(stdin.readLineSync()!);
-  print(num * num);
-}
--- task_lecture3/test/task_lecture3_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:task_lecture3/task_lecture3.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
