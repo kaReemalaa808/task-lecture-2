84d185bb6c9a580015e07ad9cb40171449ee8672
kaReem Alaa <kareemalaabakry@gmail.com>
2026-04-18 22:34:00 +0200

    0

---
+++ task_lecture3/bin/task_1.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+
+void main() {
+  int num;
+  num = int.parse(stdin.readLineSync()!);
+  print(num * num);
+}
---
+++ task_lecture3/test/task_lecture3_test.dart
@@ -0,0 +1,8 @@
+import 'package:task_lecture3/task_lecture3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
